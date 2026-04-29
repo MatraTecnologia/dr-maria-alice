8242e461bb421bef440b531398a9af85243f10a7
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2026-04-29 13:08:11 -0300

    feat: add root layout with Clerk integration, SEO metadata, and Google Tag Manager support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
+import Script from "next/script";
 import GoogleAdsTag from "@/components/GoogleAdsTag";
 import SchemaOrg from "@/components/SchemaOrg";
 import "./globals.css";
@@ -75,8 +76,26 @@ const RootLayout = ({
         <head>
           <link rel="icon" href="/favicon.ico" />
           <SchemaOrg />
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PQJRPSLN');
+            `}
+          </Script>
         </head>
         <body className={`${inter.variable} antialiased`}>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PQJRPSLN"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <GoogleAdsTag />
           {children}
         </body>
